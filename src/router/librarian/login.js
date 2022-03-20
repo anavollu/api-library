3c28dbf606f9422e36e0220b524bf4216e95856a
dellubu <dellubu>
2022-03-20 15:52:48 -0300

    feat: add celebrate to login

--- src/router/librarian/login.js
+++ src/router/librarian/login.js
@@ -1,14 +1,20 @@
 const bcrypt = require('bcrypt')
+const { celebrate, Segments, Joi } = require('celebrate')
 var jwt = require('jsonwebtoken')
 const auth = require('../../lib/auth')
 const Librarian = require('../../models/librarian')
 
 module.exports = [
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      email: Joi.string().min(5).required(),
+      password: Joi.string().min(5).required(),
+    }),
+  }),
   async (req, res, next) => {
     try {
       const { email, password } = req.body
       const found = await Librarian.findOne({ email })
-      console.log(found, req.body)
       if (!found) {
         return next({ status: 400 })
       }
